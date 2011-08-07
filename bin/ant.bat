5c9e57470c7c8bf3ff239fe61419fd86e9963d38
voidy21 <y2.void+github@gmail.com>
2011-08-07 22:18:57 +0900

    Windows bug fix

--- bin/ant.bat
+++ bin/ant.bat
@@ -1,6 +1,6 @@
 set SCRIPT_DIR=%~dp0
 set SELENIUM_TEST_BROWSER=%1
-set ANT_HOME=%SCRIPT_DIR%bin\apache-ant-1.8.2/
+set ANT_HOME=%SCRIPT_DIR%apache-ant-1.8.2/
 set PATH=%PATH%;%ANT_HOME%\bin
 shift
-ant %*
+ant %1 %2
